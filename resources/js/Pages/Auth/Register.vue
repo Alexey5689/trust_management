a404f4c0646eef00dc33b3d81f373635848341e5
efrdenvas <denisefremoff@mail.ru>
2024-10-11 12:06:09 +0300

    Начало

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -33,7 +33,39 @@ const submit = () => {
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="submit">
                             <div>
-                                <InputLabel for="name" value="Name" />
+                                <InputLabel for="name" value="Фамилия" />
+
+                                <TextInput
+                                    id="name"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    v-model="form.name"
+                                    required
+                                    autofocus
+                                    autocomplete="name"
+                                />
+
+                                <InputError class="mt-2" :message="form.errors.name" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="name" value="Имя" />
+
+                                <TextInput
+                                    id="name"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    v-model="form.name"
+                                    required
+                                    autofocus
+                                    autocomplete="name"
+                                />
+
+                                <InputError class="mt-2" :message="form.errors.name" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="name" value="Отчество" />
 
                                 <TextInput
                                     id="name"
@@ -63,7 +95,7 @@ const submit = () => {
                                 <InputError class="mt-2" :message="form.errors.email" />
                             </div>
 
-                            <div class="mt-4">
+                            <!-- <div class="mt-4">
                                 <InputLabel for="password" value="Password" />
 
                                 <TextInput
@@ -91,17 +123,17 @@ const submit = () => {
                                 />
 
                                 <InputError class="mt-2" :message="form.errors.password_confirmation" />
-                            </div>
+                            </div> -->
 
                             <div class="flex items-center justify-end mt-4">
-                                <Link
+                                <!-- <Link
                                     :href="route('login')"
                                     class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
                                     Already registered?
-                                </Link>
+                                </Link> -->
 
-                                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                <PrimaryButton class="mt-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                                     Register
                                 </PrimaryButton>
                             </div>
