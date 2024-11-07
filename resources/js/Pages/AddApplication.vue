14ce06cea5776c2ce6cf815ecf641fee652ed707
Alexey5689 <jjookkeerrr56@gmail.com>
2024-11-07 23:19:39 +0500

    TM-001D выплата раньше срока

--- resources/js/Pages/AddApplication.vue
+++ resources/js/Pages/AddApplication.vue
@@ -4,8 +4,9 @@ import TextInput from '@/Components/TextInput.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Radio from '@/Components/Radio.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
-
+import { parseISO, differenceInYears, format } from 'date-fns';
 import { ref } from 'vue';
 
 const props = defineProps({
@@ -19,10 +20,13 @@ const props = defineProps({
     },
 })
 
+
+const selectedValue = ref('');
+
 const form = useForm({
     user_id: '',
     contract_number: null,
-    // sum: null,
+    condition: selectedValue.value,
     // deadline: '', // Срок договора
     // procent: '', // Процентная ставка
     // agree_with_terms: false, // Для чекбокса
@@ -31,10 +35,31 @@ const form = useForm({
     // payments:'', // Выплаты
 });
 const userContract = ref({});
+const sum = ref(null);
+const dividends = ref(null);
+const create_date = ref('');
+const term = ref('');
+const procent = ref(null);
 
-const handleGetInfo = (id) => {
+
+const handleGetClient = (id) => {
     userContract.value = props.clients.find((client) => client.id === id);
-    console.log(userContract.value.user_contracts);
+}
+const getYearDifference =(startDate, endDate)=> {
+      return differenceInYears(parseISO(endDate), parseISO(startDate)); // Разница в годах
+}
+const handleGetContract = (contract_number) => {
+    sum.value = userContract.value.user_contracts.find((contract) => contract.contract_number === contract_number).sum;
+    let tmpCreate = userContract.value.user_contracts.find((contract) => contract.contract_number === contract_number).create_date;
+    let tmpDeadline = userContract.value.user_contracts.find((contract) => contract.contract_number === contract_number).deadline;
+    procent.value = userContract.value.user_contracts.find((contract) => contract.contract_number === contract_number).procent;
+
+    if (getYearDifference(tmpCreate, tmpDeadline) === 2){ dividends.value = ((sum.value*(procent.value/100))/12)*24}
+    else if (getYearDifference(tmpCreate, tmpDeadline) === 1) {dividends.value = ((sum.value*(procent.value/100))/12)*12}
+    else{ dividends.value = ((sum.value*(procent.value/100))/12)*36}
+
+    create_date.value = format(parseISO(tmpCreate), 'MM/dd/yyyy');
+    term.value = getYearDifference(tmpCreate, tmpDeadline) === 1 ? getYearDifference(tmpCreate, tmpDeadline) + ' год' : getYearDifference(tmpCreate, tmpDeadline) + ' года';
 }
 </script>
 <template>
@@ -52,7 +77,7 @@ const handleGetInfo = (id) => {
                                     {{ props.clients }}
                                     <InputLabel for="manager" value="Выберите клиента*" />
                                     <select id="manager" v-model="form.user_id" required
-                                        @change="handleGetInfo(form.user_id)"
+                                        @change="handleGetClient(form.user_id)"
                                         class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                                         <option value="" disabled>Выберите клиента</option>
                                         <!-- Выводим список менеджеров -->
@@ -64,18 +89,56 @@ const handleGetInfo = (id) => {
                             </div>
                             <div class="mt-4" >
                                     <InputLabel for="contract_number" value="Выберите номер договора*" />
-                                    <select id="contract_number" v-model="form.contract_number" required
-                                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-                                        <option value="" disabled>Выберите номер договора</option>
-                                        <!-- Выводим список менеджеров -->
+                                    <select id="contract_number" v-model="form.contract_number"
+                                        @change="handleGetContract(form.contract_number)"
+                                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required>
+                                        <option value="" disabled> Выберите номер договора</option>
                                         <option v-for="contract in userContract.user_contracts" :key="contract.id" :value="contract.contract_number">
                                              {{ contract.contract_number }}
                                         </option>
                                     </select>
                                     <InputError class="mt-2" :message="form.errors.contract_number" />
                             </div>
+                            <div class="mt-4">
+                                <InputLabel for="sum" value="Дата заключения*" />
+                                <p id="sum" type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">{{ create_date }}</p>
+                            </div>
+                            <div class="mt-4">
+                                <InputLabel for="sum" value="Срок договора" />
+                                <p id="sum" type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">{{ term }}</p>
+                            </div>
+                            <div class="mt-4">
+                                <InputLabel for="sum" value="Ставка" />
+                                <p id="sum" type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">{{ procent }}%</p>
+                            </div>
+                            <div class="mt-4">
+                                <InputLabel for="sum" value="Основная сумма*" />
+                                <p id="sum" type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">{{ sum }}</p>
+                            </div>
+                            <div class="mt-4">
+                                <InputLabel for="dividends" value="Дивиденды*" />
+                                <p id="dividends" type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">{{ dividends }}</p>
+                            </div>
+                            <div class="mt-4">
+                                <p>Условия списания</p>
+                                <Radio :checked="selectedValue" value="Раньше срока" @update:checked="selectedValue = $event" /> Раньше срока
+                                <Radio :checked="selectedValue" value="В срок" @update:checked="selectedValue = $event" /> В срок
+                            </div>
+                            <div v-if="selectedValue === 'Раньше срока'" class="mt-4">
+                                <p>Вывод средств</p>
+                                <div class="mt-4">
+                                    <InputLabel for="sum" value="Сумма списания" />
+                                    <p id="sum" type="text" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500">{{ sum }}</p>
+                                </div>
+                                <div class="mt-4">
+                                    <InputLabel for="create_date" value="Дата планируемой выплаты" />
+                                    <input id="create_date" type="date" v-model="form.create_date" required
+                                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+                                    <InputError class="mt-2" :message="form.errors.create_date" />
+                                </div>
+                            </div>
+
                         </form>
-                        {{ userContract.value }}
                     </div>
                 </div>
             </div>
