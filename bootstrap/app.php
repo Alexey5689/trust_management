3a5e100c52e6d8a16abe4edd741e12f0eec876f1
blankbuffoon <aterepin@yandex.ru>
2025-02-13 22:03:23 +0300

    Test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use \App\Http\Middleware\RoleMiddleware;
 use \App\Http\Middleware\CheckUserStatus;
+use Symfony\Component\HttpFoundation\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,6 +22,16 @@ return Application::configure(basePath: dirname(__DIR__))
             'role'=>RoleMiddleware::class,
             'checkActive' => CheckUserStatus::class,
         ]);
+        $middleware->trustProxies(at: [
+            '192.168.1.1',
+            '10.0.0.0/8',
+        ]);
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
 
 
         //
