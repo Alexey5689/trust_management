f0ace9772f64410b3cea8086d10ad7718bfa8dfd
alex89 <alex.dya4encko89@yandex.ru>
2025-02-14 23:23:31 +0500

    test5

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://api-tm.lb.pro-technologii.ru'),
 
     /*
     |--------------------------------------------------------------------------
