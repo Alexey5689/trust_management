ceee3ca4876a555e8b15001bb9d85a7bfd74598f
alex89 <alex.dya4encko89@yandex.ru>
2025-02-15 16:12:11 +0500

    test12

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,4 +1,6 @@
+import { Ziggy as ZiggyBase } from '../../vendor/tightenco/ziggy';
+
 export const Ziggy = {
+    ...ZiggyBase,
     url: import.meta.env.VITE_URL || 'https://api-tm.lb.pro-technologii.ru',
-    routes: {},
 };
