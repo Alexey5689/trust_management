2994e574f77d8fa9a779ce82bc030963f1d2a07c
blankbuffoon <aterepin@yandex.ru>
2025-02-18 17:00:00 +0300

    Revert "test6"
    This reverts commit 4d84d2cf853eb8e08cb3e126b8b7649034707d2a.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { Ziggy } from '@/ziggy'; // Указываем путь к Ziggy
-
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,7 +14,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .use(ZiggyVue)
             .mount(el);
     },
     progress: {
--- resources/js/ziggy.js
+++
@@ -1,4 +0,0 @@
-export const Ziggy = {
-    url: import.meta.env.VITE_URL || 'https://api-tm.lb.pro-technologii.ru',
-    routes: {},
-};
