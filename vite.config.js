25cf0e53c1a49f59525bb1c5301efa5bc3909eca
blankbuffoon <aterepin@yandex.ru>
2025-02-18 16:59:58 +0300

    Revert "test8"
    This reverts commit 9306445f0187a32465d860ac852476eae765e8b4.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
-        https: true,  // Выключаем HTTPS
+        https: false,  // Выключаем HTTPS
     },
     plugins: [
         laravel({
