63fdb5e8eb9aa3fed14f696c5dc038b00a86910a
blankbuffoon <aterepin@yandex.ru>
2025-02-18 20:21:41 +0300

    Fix laravel/vue routing

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/index.js';
+import { ZiggyVue } from 'ziggy-js';
 import { Ziggy } from "@/ziggy.js";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -15,10 +15,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, {
-                ...Ziggy,
-                location: new URL(import.meta.env.VITE_APP_URL || 'https://api-tm.lb.pro-technologii.ru'),
-            })
+            .use(ZiggyVue, Ziggy)
             .mount(el);
     },
     progress: {
