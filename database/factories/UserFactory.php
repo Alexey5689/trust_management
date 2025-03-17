268b421f89f9825e305f9fc88baf75fd79daf6a3
alex89 <alex.dya4encko89@yandex.ru>
2025-03-17 19:37:08 +0500

    del commented code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,36 +23,8 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        
-        // return [
-        //     'first_name' => fake()->firstName(),
-        //     'last_name' => fake()->lastName(),
-        //     'middle_name' => fake()->firstName(),
-        //     'phone_number' => "8" . rand(1000000000, 9999999999),
-        //     'email' => fake()->unique()->safeEmail(),
-        //     'password' => static::$password ??= Hash::make('password'),
-        //     'token' => Str::random(60), // Генерация случайного токена
-        //     'refresh_token' => Str::random(60),
-        //     'role_id' => null, // Добавляем role_id, если это используется
-        // ];
-
         return [
             
         ];
     }
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    // public function unverified(): static
-    // {
-    //     return $this->state(fn (array $attributes) => [
-    //         'email_verified_at' => null,
-    //     ]);
-    // }
-    // public function admin(): static
-    // {
-    //     return $this->state(fn (array $attributes) => [
-    //         'role_id' => 1, // Предположим, что роль администратора имеет ID = 1
-    //     ]);
-    // }
 }
